87a5f1baf35240a73ed0193b57f3194db5071d64
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-03-18 22:21:45 +0100

    initial checkin

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { resolve } = require('path');
+const { readFileSync } = require('fs');
+const { name } = JSON.parse(readFileSync(resolve(__dirname, 'package.json')));
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    library: name,
+    libraryTarget: 'umd',
+    path: resolve(__dirname, 'dist'),
+  },
+};
